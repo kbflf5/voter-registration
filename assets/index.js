ed880d3f6ba1506b3332858e939060b277c952fb
kbfl-campaign <kbfl-campaign@kbfl.org>
2016-05-03 02:52:04 +0800

    Freeze the counter

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,5 @@
 $(function () {
-  window.setInterval(function () {
+  /*window.setInterval(function () {
     var t = (Math.floor(Date.now()/1000)-1461686400)/86400;
     var formGenerated = (
       8500+(
@@ -15,7 +15,7 @@ $(function () {
     var seatFormRatio =  Math.round(formGenerated/47300*100*10)/10 ;
     $('.formGenerated').text(formGenerated.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
     $('.seatFormRatio').text(seatFormRatio.toString()+"%");
-  }, 1000);
+  }, 1000);*/
 
   var parent = $(".partner-container");
   var divs = parent.children();
